9427ff533c2ce4a71694ef7e92f43035929ed472
nikesh16 <nikeshacharay2@gmail.com>
2022-11-16 03:23:22 +0300

    V2.0

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    rules: {
+        "no-unsued-vars": "off",
+        "color-function-notation": null,
+    },
+    "parserOptions": {
+        "ecmaVersion": 6
+    }
+}
--- app.js
+++ app.js
@@ -1,61 +1,34 @@
-var mouseCursor = document.querySelector('.cursor');
+let mouseCursor = document.querySelector('.cursor')
+window.addEventListener('mousemove', cursor)
+function cursor (e) {
+  mouseCursor.style.top = e.pageX
+  mouseCursor.style.left = pageX}
 
+const imgBx = document.querySelector('.imgBx')
+const slides = imgBx.getElementsByTagName('img')
+console.log(slides)
+let i = 0
 
-window.addEventListener('mousemove',cursor);
-
-function cursor(e){
-    mouseCursor.style.top = e.pageY + 'px';
-    mouseCursor.style.left = e.pageX + "px";
+function nextSlide () {
+  slides[i].classList.remove('active')
+  i = (i + 1) % slides.length
+  slides[i].classList.add('active')
 }
 
-const imgBx = document.querySelector(".imgBx");
-const slides = imgBx.getElementsByTagName("img");
-console.log(slides);
-let i = 0;
+const sideMenu = document.getElementById('sideMenu')
 
-function nextSlide() {
-    slides[i].classList.remove("active");
-    i = (i + 1) % slides.length;
-    slides[i].classList.add("active");
+function openMenu () {
+  sideMenu.style.right = '0'
+}
+function closeMenu() {
+  sideMenu.style.right = '-200px'
 }
 
-function prevSlide() {
-    slides[i].classList.remove("active");
-    i = (i - 1 + slides.length) % slides.length;
-    slides[i].classList.add("active");
+const contactDetails = document.getElementById('contact-details')
+
+function contact () {
+  contactDetails.innerText = 'I am currenctly living in Vyazemskiy Pereulok, Saint Petersburg. You can contact me directly by Whatsapp. For that click on the icons.'
 }
-
-let sideMenu = document.getElementById("sideMenu");
-
-function openMenu(){
-    sideMenu.style.right = "0";
-
-}
-function closeMenu(){
-    sideMenu.style.right = "-200px";
-
-}
-
-
-
-// // const form = document.forms['submit-to-google-sheet']
-
-// form.addEventListener('submit', e => {
-//   e.preventDefault()
-//   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-//     .then(response => console.log('Success!', response))
-//     .catch(error => console.error('Error!', error.message))
-// })
-
-
-let contactDetails = document.getElementById('contact-details');
-
-
-function contact(){
-    contactDetails.innerText= "I am currenctly living in Vyazemskiy Pereulok, Saint Petersburg. You can contact me directly by Whatsapp. For that click on the icons.";
-
-}
-contact();
-
-let skills = document.getElementById("skills");
-skills.innerHTML = "Skills: Html, CSS, Adobe PhotoShop, Adobe Premiere Pro, Microsoft Office, Microsoft Office Packages, Java, python, JS ";
+contact()
+const skills = document.getElementById('skills')
+skills.innerHTML = 'Skills: Html, CSS, Adobe PhotoShop, Adobe Premiere Pro, Microsoft Office, Microsoft Office Packages, Java, python, JS'
