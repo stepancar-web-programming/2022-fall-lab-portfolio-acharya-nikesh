620231e551e44ee2e81dc0c2c7bc9ed725f045da
GG <zjjhgzh@gmail.com>
2022-11-23 14:50:27 +0300

    update

--- app.js
+++ app.js
@@ -1,40 +1,58 @@
-let mouseCursor = document.querySelector('.cursor')
+/* eslint-env jquery */
+
+$(window).on('load', function () {
+  jQuery('.loader').fadeOut(1800)
+  jQuery('.navbar').fadeIn(1800)
+  jQuery('li').fadeIn(7000)
+  jQuery('span').fadeIn(7000)
+  jQuery('.cursor').fadeIn(6000)
+  jQuery('#header').fadeIn(6000)
+  jQuery('.wrapper').fadeIn(6000)
+  jQuery('.contact-left').fadeIn(6000)
+  jQuery('.contact-right').fadeIn(6000)
+  jQuery('#sliding').fadeIn(6000)
+  jQuery('#about').fadeIn(6000)
+})
+const mouseCursor = document.querySelector('.cursor')
 window.addEventListener('mousemove', cursor)
 function cursor (e) {
-  mouseCursor.style.top = e.pageX
-  mouseCursor.style.left = e.pageX}
+  mouseCursor.style.top = e.pageY + 'px'
+  mouseCursor.style.left = e.pageX + 'px'
+}
 
 const imgBx = document.querySelector('.imgBx')
 const slides = imgBx.getElementsByTagName('img')
 console.log(slides)
 let i = 0
 
-function nextSlide () {
+function nextSlide () { // eslint-disable-line no-unused-vars
   slides[i].classList.remove('active')
   i = (i + 1) % slides.length
   slides[i].classList.add('active')
 }
 
-function prevSlide () {
-  slides[i].classList.add('active')
-  i = (i+2 ) % slides.length
+function prevSlide () { // eslint-disable-line no-unused-vars
   slides[i].classList.remove('active')
+  i = ((i - 1) === -1) ? 2 : (i - 1) % slides.length
+  slides[i].classList.add('active')
 }
 
 const sideMenu = document.getElementById('sideMenu')
 
-function openMenu () {
+function openMenu () { // eslint-disable-line no-unused-vars
   sideMenu.style.right = '0'
 }
-function closeMenu() {
+function closeMenu () { // eslint-disable-line no-unused-vars
   sideMenu.style.right = '-200px'
 }
 
 const contactDetails = document.getElementById('contact-details')
 
 function contact () {
-  contactDetails.innerText = 'I am currenctly living in Vyazemskiy Pereulok, Saint Petersburg. You can contact me directly by Whatsapp. For that click on the icons.'
+  contactDetails.innerText =
+    'I am currenctly living in Vyazemskiy Pereulok, Saint Petersburg. You can contact me directly by Whatsapp. For that click on the icons.'
 }
 contact()
 const skills = document.getElementById('skills')
-skills.innerHTML = 'Skills: Html, CSS, Adobe PhotoShop, Adobe Premiere Pro, Microsoft Office, Microsoft Office Packages, Java, python, JS'
+skills.innerHTML =
+  'Skills: Html, CSS, Adobe PhotoShop, Adobe Premiere Pro, Microsoft Office, Microsoft Office Packages, Java, python, JS'
